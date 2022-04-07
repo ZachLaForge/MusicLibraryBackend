a6cbf33b449d224c3b258481c6e28d13c85c3dd8
Zachary LaForge <zach.laforge@hotmail.com>
2022-04-07 14:47:12 -0400

    crteated new views functions + added new urls path for song_detail

--- music/views.py
+++ music/views.py
@@ -1,8 +1,10 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import SongSerializer
 from .models import Song
 from rest_framework import status
+
 
 # Create your views here.
 @api_view(['GET', 'POST'])
@@ -20,3 +22,23 @@ def songs_list(request):
 
     else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)  
+
+@api_view(['GET','PUT','DELETE'])
+def song_detail(request, pk):
+    song = get_object_or_404(Song, pk=pk)
+
+    if request.method == "GET":
+        serializer = SongSerializer(song)
+        return Response(serializer.data)
+
+
+    elif request.method == 'PUT':
+        serializer = SongSerializer(song, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+
+    elif request.method == 'DELETE':
+        song.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)

e0b55459845d638e48f01014fb85ed6fda881320
Zachary LaForge <zach.laforge@hotmail.com>
2022-04-07 14:25:40 -0400

    registered Song model w/ admin site + completed post functionality for Song + Updated restframework decimal settings ( Commented out currently )

--- music/views.py
+++ music/views.py
@@ -1,9 +1,22 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from .serializers import SongSerializer
+from .models import Song
+from rest_framework import status
 
 # Create your views here.
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def songs_list(request):
+    if request.method == 'GET':
+        song = Song.objects.all()
+        serializer = SongSerializer(song, many=True)
+        return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        serializer = SongSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-    return Response('ok')
+    else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)  
